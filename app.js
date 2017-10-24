40f794ae608d02215a1da185eedb93c702f5ace5
arintaosorio <arintaosorio@gmail.com>
2017-10-24 09:12:12 -0500

    Hola, subiendo mi primer README.md con imagen

---
+++ app.js
@@ -0,0 +1,37 @@
+/*
+Mi solución
+
+function areArraysSame (array1,array2) {
+  
+  if (array1.length === array2.length){ 
+    
+    for (var i=0; i<array2.length; i++){
+      
+     if (array1[i] !== array2[i])
+    
+    return false;
+  }
+  
+    return true;
+    
+}
+return false
+}
+
+  
+ console.log(areArraysSame([1,2,4],[2,1,2,3]))
+*/
+
+var comparar = function(arreglo1,arreglo2){
+if (arreglo1.length === arreglo2){
+	return false
+} else{
+	for (var i=0; i<arreglo1.length; i++){
+
+}
+}
+ 
+ var array1 = [1,2,3,4,5,6,7,8,9,18];
+ var array2 = [1,2,3,4,5,6,7,8,9,18];
+
+comparar(array1,array2);
